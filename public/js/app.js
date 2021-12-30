2f4f176b9af41ed8b07e35968c0fba9c7bf27387
hasnat-hybrid <hasnaties@gmail.com>
2021-12-30 21:40:25 +0500

    Unnecesssary code removed from public app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('This is client side JS. file.');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#messageOne');
